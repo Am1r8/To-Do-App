49bed062604a47f62ec636b842056271b1594b12
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2021-09-17 17:12:28 -0400

    Add files via upload

---
+++ precache-manifest.c213b11a9c21cd74c7905f4094a9aa6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed329beb42d7c403c057817c37968a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d36817433e6dbdc2a8bd",
+    "url": "/static/css/main.62548029.chunk.css"
+  },
+  {
+    "revision": "8afab9cd91431c5d0ef1",
+    "url": "/static/js/2.9e6bd4b2.chunk.js"
+  },
+  {
+    "revision": "d36817433e6dbdc2a8bd",
+    "url": "/static/js/main.ec7a5026.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
